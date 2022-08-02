c5721761b62b7538386455a5677011210b57db88
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-08-02 11:50:50 +0200

    RcppDeepState parameters definition

--- R/ci_setup.R
+++ R/ci_setup.R
@@ -1,7 +1,10 @@
 ##' @title  travis setup
 ##' @param repository path to the repository root folder
 ##' @export
-ci_setup<-function(repository="./", event="pull_request"){
+ci_setup <- function(repository="./", event="pull_request", 
+                     fail_ci_if_error=FALSE, location="/", seed="-1", 
+                     time_limit="2", max_inputs="3", comment=FALSE){
+                    
   workflow_path <- gsub("[/]+", "/", file.path(repository, ".github/workflows"))
   workflow_file <- file.path(workflow_path, "RcppDeepState.yaml")
   
@@ -27,11 +30,23 @@ ci_setup<-function(repository="./", event="pull_request"){
 
   name <- "name: 'RcppDeepState analysis'"
 
-  # jobs definition
+  # checkout step
   checkout_step <- paste0(indent(3), "- uses: ", "actions/checkout@v2", "\n\n")
-  rcppdeepstate_step <- paste0(indent(3), "- uses: ", action_repo, "\n",
-                               indent(4), "with:\n")
+    
+  # RcppDeepState step
+  fail_ci_param <- get_param_yaml("fail_ci_if_error", tolower(fail_ci_if_error))
+  location_param <- get_param_yaml("location", location)
+  seed_param <- get_param_yaml("seed", seed)
+  time_limit_param <- get_param_yaml("time_limit", time_limit)
+  max_inputs_param <- get_param_yaml("max_inputs", max_inputs)
+  comment_param <- get_param_yaml("comment", tolower(comment))
 
+  params <- paste(fail_ci_param, location_param, seed_param, time_limit_param,
+                  max_inputs_param, comment_param, sep="\n")
+  rcppdeepstate_step <- paste0(indent(3), "- uses: ", action_repo, "\n",
+                               indent(4), "with:\n", params)
+
+  # jobs definition
   steps <- paste0(indent(2), "steps: ", "\n", checkout_step, rcppdeepstate_step)
 
   env <- paste0(indent(2), "env:", "\n", indent(3), 
@@ -48,7 +63,14 @@ ci_setup<-function(repository="./", event="pull_request"){
 }
 
 
+# Helper function that returns 'times' indentation symbols
 indent <- function(times=1){
   indent_symbol <- "  "
   strrep(indent_symbol, times)
+}
+
+# Helper function that generates a string representing a yaml formatted code of
+# a given parameter
+get_param_yaml <- function(param_name, param){
+  paste0(indent(5), param_name, ": '", param, "'")
 }
